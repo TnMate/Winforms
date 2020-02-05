81a9a129b8a273992524297feef380e736829655
Trembickij Máté <tnmate@hotmail.com>
2020-02-05 10:13:04 +0100

    Uploading my tetris

---
+++ Tetris/Tetris/View/TetrisForm.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace Tetris
+{
+    partial class TetrisForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sizeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.smallToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mediumToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.largeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pauseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.TimeText = new System.Windows.Forms.ToolStripStatusLabel();
+            this._actualTime = new System.Windows.Forms.ToolStripStatusLabel();
+            this._openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this._saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.optionsToolStripMenuItem,
+            this.pauseToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(600, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // optionsToolStripMenuItem
+            // 
+            this.optionsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newGameToolStripMenuItem,
+            this.sizeToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.loadToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
+            this.optionsToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.optionsToolStripMenuItem.Text = "Options";
+            // 
+            // newGameToolStripMenuItem
+            // 
+            this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
+            this.newGameToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.newGameToolStripMenuItem.Text = "New Game";
+            this.newGameToolStripMenuItem.Click += new System.EventHandler(this.newGameToolStripMenuItem_Click);
+            // 
+            // sizeToolStripMenuItem
+            // 
+            this.sizeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.smallToolStripMenuItem,
+            this.mediumToolStripMenuItem,
+            this.largeToolStripMenuItem});
+            this.sizeToolStripMenuItem.Name = "sizeToolStripMenuItem";
+            this.sizeToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.sizeToolStripMenuItem.Text = "Size";
+            // 
+            // smallToolStripMenuItem
+            // 
+            this.smallToolStripMenuItem.Name = "smallToolStripMenuItem";
+            this.smallToolStripMenuItem.Size = new System.Drawing.Size(119, 22);
+            this.smallToolStripMenuItem.Text = "Small";
+            this.smallToolStripMenuItem.Click += new System.EventHandler(this.smallToolStripMenuItem_Click);
+            // 
+            // mediumToolStripMenuItem
+            // 
+            this.mediumToolStripMenuItem.Name = "mediumToolStripMenuItem";
+            this.mediumToolStripMenuItem.Size = new System.Drawing.Size(119, 22);
+            this.mediumToolStripMenuItem.Text = "Medium";
+            this.mediumToolStripMenuItem.Click += new System.EventHandler(this.mediumToolStripMenuItem_Click);
+            // 
+            // largeToolStripMenuItem
+            // 
+            this.largeToolStripMenuItem.Name = "largeToolStripMenuItem";
+            this.largeToolStripMenuItem.Size = new System.Drawing.Size(119, 22);
+            this.largeToolStripMenuItem.Text = "Large";
+            this.largeToolStripMenuItem.Click += new System.EventHandler(this.largeToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // loadToolStripMenuItem
+            // 
+            this.loadToolStripMenuItem.Name = "loadToolStripMenuItem";
+            this.loadToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.loadToolStripMenuItem.Text = "Load";
+            this.loadToolStripMenuItem.Click += new System.EventHandler(this.loadToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // pauseToolStripMenuItem
+            // 
+            this.pauseToolStripMenuItem.Name = "pauseToolStripMenuItem";
+            this.pauseToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.pauseToolStripMenuItem.Text = "Pause";
+            this.pauseToolStripMenuItem.Click += new System.EventHandler(this.pauseToolStripMenuItem_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.TimeText,
+            this._actualTime});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 344);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Padding = new System.Windows.Forms.Padding(1, 0, 10, 0);
+            this.statusStrip1.Size = new System.Drawing.Size(600, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // TimeText
+            // 
+            this.TimeText.Name = "TimeText";
+            this.TimeText.Size = new System.Drawing.Size(40, 17);
+            this.TimeText.Text = "Time :";
+            // 
+            // _actualTime
+            // 
+            this._actualTime.Name = "_actualTime";
+            this._actualTime.Size = new System.Drawing.Size(13, 17);
+            this._actualTime.Text = "0";
+            // 
+            // _openFileDialog
+            // 
+            this._openFileDialog.FileName = "_openFileDialog";
+            this._openFileDialog.Filter = "Tetris Table (*.tt)|*.tt";
+            // 
+            // _saveFileDialog
+            // 
+            this._saveFileDialog.Filter = "Tetris Table (*.tt)|*.tt";
+            // 
+            // TetrisForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "TetrisForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.TetrisForm_Load_1);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem optionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newGameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sizeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel TimeText;
+        private System.Windows.Forms.ToolStripStatusLabel _actualTime;
+        private System.Windows.Forms.ToolStripMenuItem smallToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mediumToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem largeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pauseToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog _openFileDialog;
+        private System.Windows.Forms.SaveFileDialog _saveFileDialog;
+    }
+}
+
---
+++ Tetris/Tetris/View/TetrisForm.cs
@@ -0,0 +1,332 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using Tetris.Model;
+using Tetris.Persistence;
+
+namespace Tetris
+{
+    public partial class TetrisForm : Form
+    {
+
+        #region Fields
+
+        private TetrisDataAccessInterface _dataAccess;
+        private TetrisModel _model;
+        private Button[,] _buttonGrid;
+        private Timer _timer;
+        private Boolean _timerActive;
+
+        #endregion
+
+        #region Constructors
+
+        public TetrisForm()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Form Event Handler
+
+        private void TetrisForm_Load_1(object sender, EventArgs e)
+        {
+            _dataAccess = new TetrisFileDataAccess();
+
+            _model = new TetrisModel(_dataAccess);
+            _model.GameAdvanced += new EventHandler<TetrisEventArgs>(model_gameAdvanced);
+            _model.GameOver += new EventHandler<TetrisEventArgs>(model_gameOver);
+
+            KeyPreview = true;
+            KeyDown += new KeyEventHandler(TetrisForm_KeyDown);
+
+            NewGame();
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private void GenerateTable()
+        {
+            if (_buttonGrid != null)
+            {
+                foreach (Button button in _buttonGrid)
+                {
+                    Controls.Remove(button);
+                }
+            }
+
+            _buttonGrid = new Button[_model.getTableSize(), 16];
+            for (Int32 i = 0; i < _model.getTableSize(); i++)
+                for (Int32 j = 0; j < 16; j++)
+                {
+                    _buttonGrid[i, j] = new Button();
+                    _buttonGrid[i, j].BackColor = Color.White;
+                    _buttonGrid[i, j].Location = new Point(5 + 25 * j, 25 + 25 * i);
+                    _buttonGrid[i, j].Size = new Size(25, 25);
+                    _buttonGrid[i, j].Enabled = false; 
+                    _buttonGrid[i, j].TabIndex = 100 + i * _model.getTableSize() + j;
+                    _buttonGrid[i, j].FlatStyle = FlatStyle.Flat;
+
+                    Controls.Add(_buttonGrid[i, j]);
+                }
+        }
+
+        private void SetupWindow()
+        {
+            switch (_model.getTableSize())
+            {
+                case 4:
+                    this.Width = 425;
+                    this.Height = 200;
+                    break;
+                case 8:
+                    this.Width = 425;
+                    this.Height = 300;
+                    break;
+                case 12:
+                    this.Width = 425;
+                    this.Height = 400;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void NewGame()
+        {
+            _model.newGame();
+            SetupWindow();
+            GenerateTable();
+            if (_timer!=null)
+                _timer.Dispose();
+
+            _timer = new Timer();
+            _timer.Interval = 1000;
+            _timer.Tick += new EventHandler(Timer_Tick);
+            _timer.Start();
+            _timerActive = true;
+
+            _actualTime.Text = _model.getTime().ToString();
+            SetupCurrentTableState();
+
+            pauseToolStripMenuItem.Enabled = true;
+        }
+
+        private void SetupCurrentTableState()
+        {
+            if (_buttonGrid != null)
+            {
+                var map = _model.getTable();
+
+                for (Int32 i = 0; i < _model.getTableSize(); i++)
+                    for (Int32 j = 0; j < 16; j++)
+                    {
+                        if (map[i + 1, j] == 1)
+                        {
+                            _buttonGrid[i, j].BackColor = Color.Purple;
+                        }
+                        else if (map[i + 1, j] == 2)
+                        {
+                            _buttonGrid[i, j].BackColor = Color.Orange;
+                        }
+                        else if (map[i + 1, j] == 3)
+                        {
+                            _buttonGrid[i, j].BackColor = Color.Green;
+                        }
+                        else if (map[i + 1, j] == 4)
+                        {
+                            _buttonGrid[i, j].BackColor = Color.Yellow;
+                        }
+                        else if (map[i + 1, j] == 5)
+                        {
+                            _buttonGrid[i, j].BackColor = Color.Red;
+                        }
+                        else if (map[i + 1, j] == 6)
+                        {
+                            _buttonGrid[i, j].BackColor = Color.Blue;
+                        }
+                        else if (map[i + 1, j] == 7)
+                        {
+                            _buttonGrid[i, j].BackColor = Color.Turquoise;
+                        }
+                        else
+                        {
+                            _buttonGrid[i, j].BackColor = Color.White;
+                        }
+                    }
+            }
+        }
+
+        #endregion
+
+        #region Timer event handlers
+
+        private void Timer_Tick(Object sender, EventArgs e)
+        {
+            _model.advanceTime();
+            _actualTime.Text = _model.getTime().ToString();
+        }
+
+        #endregion
+
+        #region Other event handlers
+
+        private void model_gameAdvanced(Object sender, TetrisEventArgs e)
+        {
+            SetupCurrentTableState();
+        }
+
+        private void model_gameOver(Object sender, TetrisEventArgs e)
+        {
+            _timer.Stop();
+            _timerActive = false;
+            pauseToolStripMenuItem.Enabled = false;
+            MessageBox.Show("Game Over!" + Environment.NewLine +
+                               e.ReturnGameTime + " time",
+                               "Tetris game",
+                               MessageBoxButtons.OK,
+                               MessageBoxIcon.Asterisk);
+        }
+
+        private void TetrisForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (_buttonGrid != null && _timerActive)
+            {
+                switch (e.KeyCode)
+                {
+                    case Keys.Left:
+                        _model.GoLeft();
+                        break;
+                    case Keys.Right:
+                        _model.GoRight();
+                        break;
+                    case Keys.Down:
+                        _model.GoDown();
+                        break;
+                    case Keys.Up:
+                        _model.RotateMeSenpai();
+                        break;
+                    default:
+                        break;
+                }
+                e.SuppressKeyPress = true;
+            }
+            SetupCurrentTableState();
+        }
+
+        #endregion
+
+        #region menu handlers
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void newGameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            NewGame();
+        }
+
+        private void smallToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            _model.SetTableSize(0);
+            NewGame();
+        }
+
+        private void mediumToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            _model.SetTableSize(1);
+            NewGame();
+        }
+
+        private void largeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            _model.SetTableSize(2);
+            NewGame();
+        }
+
+        private void pauseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (_timerActive)
+            {
+                _timer.Stop();
+                _timerActive = false;
+            }
+            else
+            {
+                _timer.Start();
+                _timerActive = true;
+            }
+        }
+
+        private async void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Boolean restartTimer = _timer.Enabled;
+            _timer.Stop();
+
+            if (_saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    await _model.SaveGameAsync(_saveFileDialog.FileName);
+                }
+                catch (TetrisDataException)
+                {
+                    MessageBox.Show("Couldn't save the game!" + Environment.NewLine + "Wrong path or you don't have the permission to write to the directory", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Couldn't save the game!" + Environment.NewLine + "You can't save a lost game!", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+            if (restartTimer)
+                _timer.Start();
+        }
+
+        private async void loadToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Boolean restartTimer = _timer.Enabled;
+            _timer.Stop();
+
+            if (_openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    await _model.LoadGameAsync(_openFileDialog.FileName);
+                    saveToolStripMenuItem.Enabled = true;
+                }
+                catch (TetrisDataException)
+                {
+                    MessageBox.Show("Couldn't load the game!" + Environment.NewLine + "Wrong path or file format", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                    _model.newGame();
+                    GenerateTable();
+                    saveToolStripMenuItem.Enabled = true;
+                    pauseToolStripMenuItem.Enabled = true;
+
+                }
+
+                SetupWindow();
+                GenerateTable();
+                SetupCurrentTableState();
+                pauseToolStripMenuItem.Enabled = true;
+
+            }
+
+            if (restartTimer)
+                _timer.Start();
+        }
+
+        #endregion
+
+        private void openFileDialog1_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
+        }
+    }
+}
